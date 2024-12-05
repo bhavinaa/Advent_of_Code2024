77f36c7bd02e97c6098772499e8a159231894238
bhavina <142370200+bhavinaa@users.noreply.github.com>
2024-12-05 10:34:10 +0800

    Day1 - python
    Attempted the 2024 advent of code challenges day 1

---
+++ code/advent_of_code_day1.py
@@ -0,0 +1,5 @@
+import urllib.request
+input = urllib.request.urlopen("https://adventofcode.com/2024/day/1/input")
+#data = open('https://adventofcode.com/2024/day/1/input', 'r').read().split('\n')
+print(input)
+
